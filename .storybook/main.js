0a076fc0b903b774cceded3794ff371d2c9de6d4
Rob Caldecott <robert.caldecott@zego.com>
2022-04-25 13:54:41 +0100

    Package updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,6 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "storybook-addon-next-router",
     "storybook-dark-mode",
   ],
